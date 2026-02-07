c541758b974357bd7825632eb03261880d608861
sudha2023-mygithub <msreddy885@gmail.com>
2026-02-07 12:23:01 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/abc/:userid", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Sudharshan", secondName: "Reddy" });
-});
+app.use(
+  "/user",
+  (req, res, then) => {
+    console.log("First route handler");
+    res.send("1st route handler");
+    then();
+  },
+  (req, res) => {
+    console.log("Second route handler");
+    res.send("2nd route handler");
+  },
+);
 
 app.listen(3000, () => {
   console.log("Server is Successfully listening on port 3000");
