73b2a1e6eb7cd0d862a756e356be906c7b7c93a2
sudha2023-mygithub <msreddy885@gmail.com>
2026-02-09 18:26:29 +0530

    dynamic data in signup API, get, delete and update APIs

--- src/app.js
+++ src/app.js
@@ -15,6 +15,46 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find({ emailId: req.body.emailId }).exec();
+    res.send(user);
+  } catch (err) {
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    //const user = await User.findByIdAndDelete(userId);
+    res.send(user);
+  } catch (err) {
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User updated Successfully");
+  } catch (err) {
+    res.send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("database connection established");
