78d33991caf6f8050728b760d96796a297632f26
sudha2023-mygithub <msreddy885@gmail.com>
2026-02-10 19:02:07 +0530

    created login api

--- src/app.js
+++ src/app.js
@@ -32,6 +32,26 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      res.send("Login Successful!!!");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
 app.get("/user", async (req, res) => {
   try {
     const user = await User.find({ emailId: req.body.emailId }).exec();
