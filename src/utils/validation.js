205c660575f84eda585e062ac42ac10a3b7d8438
sudha2023-mygithub <msreddy885@gmail.com>
2026-02-14 12:48:56 +0530

    logout and edit profile api

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -11,4 +11,22 @@ const validateSignUpData = (req) => {
   }
 };
 
-module.exports = { validateSignUpData };
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "about",
+    "photoUrl",
+    "gender",
+    "age",
+    "skills",
+  ];
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field),
+  );
+
+  return isEditAllowed;
+};
+
+module.exports = { validateSignUpData, validateEditProfileData };
